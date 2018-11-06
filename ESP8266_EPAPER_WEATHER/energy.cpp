8c3ee612cc779a0e27a5649dace065e4a8048269
maurinElectroTextile <maurin@datapaulette.org>
2018-11-06 13:46:29 +0100

    reorganized fonctions order

--- ESP8266_EPAPER_WEATHER/energy.cpp
+++ ESP8266_EPAPER_WEATHER/energy.cpp
@@ -12,79 +12,79 @@ String energyClientSecret64 = "YTBjYzMyODQtYzE5Yi00YzZjLTk1YTMtNGFmYmUxY2ViZjlhO
 
 DynamicJsonBuffer energyJsonBuffer;
 
-bool energyParseAuthToken(String payload) {
-	Serial.println("energyParseAuthToken:");
-	Serial.println(payload);
-
-	energyJsonBuffer.clear();
-	JsonObject& root = energyJsonBuffer.parseObject(payload);
-	energyAuthToken = String((const char *)root["access_token"]);
-
-	return true;
-}
-
 bool energyFetchAuthToken(HTTPClient *httpClient) {
-	bool status = false;
+  bool status = false;
 
-	if (!httpClient->begin(ENERGY_OAUTH_HOST, ENERGY_OAUTH_PORT, ENERGY_OAUTH_URL, ENERGY_OAUTH_CERT_FP)) {
-		Serial.println("energyGetAuthToken/INIT");
-		return false;
-	}
+  if (!httpClient->begin(ENERGY_OAUTH_HOST, ENERGY_OAUTH_PORT, ENERGY_OAUTH_URL, ENERGY_OAUTH_CERT_FP)) {
+    Serial.println("energyGetAuthToken/INIT");
+    return false;
+  }
 
-	httpClient->addHeader("Authorization", "Basic " + energyClientSecret64);
+  httpClient->addHeader("Authorization", "Basic " + energyClientSecret64);
 
-	int httpCode = httpClient->POST(NULL, 0);
+  int httpCode = httpClient->POST(NULL, 0);
 
-	if (httpCode > 0) {
-		status = energyParseAuthToken(httpClient->getString());
-	} else {
-		Serial.printf("energyGetAuthToken/POST: %s", httpClient->errorToString(httpCode).c_str());
-	}
-	httpClient->end();
+  if (httpCode > 0) {
+    status = energyParseAuthToken(httpClient->getString());
+  } else {
+    Serial.printf("energyGetAuthToken/POST: %s", httpClient->errorToString(httpCode).c_str());
+  }
+  httpClient->end();
 
-	return status;
+  return status;
 }
 
-bool energyParseActualGen(String payload) {
-	Serial.println("energyParseActualGen:");
-	Serial.println(payload);
+bool energyParseAuthToken(String payload) {
+  Serial.println("energyParseAuthToken:");
+  Serial.println(payload);
 
-	return true;
+  energyJsonBuffer.clear();
+  JsonObject& root = energyJsonBuffer.parseObject(payload);
+  energyAuthToken = String((const char *)root["access_token"]);
+
+  return true;
 }
 
 bool energyFetchActualGen(HTTPClient *httpClient) {
-	bool status = false;
+  bool status = false;
 
-	if (!httpClient->begin(ENERGY_API_HOST, ENERGY_API_PORT, ENERGY_API_URL, ENERGY_API_CERT_FP)) {
-		Serial.println("energyFetchActualGen/INIT");
-		return false;
-	}
+  if (!httpClient->begin(ENERGY_API_HOST, ENERGY_API_PORT, ENERGY_API_URL, ENERGY_API_CERT_FP)) {
+    Serial.println("energyFetchActualGen/INIT");
+    return false;
+  }
 
-	httpClient->addHeader("Authorization", "Bearer " + energyAuthToken);
+  httpClient->addHeader("Authorization", "Bearer " + energyAuthToken);
 
-	int httpCode = httpClient->GET();
+  int httpCode = httpClient->GET();
 
-	if (httpCode > 0) {
-		status = energyParseActualGen(httpClient->getString());
-	} else {
-		Serial.printf("energyFetchActualGen/GET: %s", httpClient->errorToString(httpCode).c_str());
-	}
-	httpClient->end();
+  if (httpCode > 0) {
+    status = energyParseActualGen(httpClient->getString());
+  } else {
+    Serial.printf("energyFetchActualGen/GET: %s", httpClient->errorToString(httpCode).c_str());
+  }
+  httpClient->end();
 
-	return status;
+  return status;
+}
+
+bool energyParseActualGen(String payload) {
+  Serial.println("energyParseActualGen:");
+  Serial.println(payload);
+
+  return true;
 }
 
 bool energyUpdate() {
-	HTTPClient httpClient;
+  HTTPClient httpClient;
 
-	httpClient.setReuse(true);
-	httpClient.setTimeout(2000);
+  httpClient.setReuse(true);
+  httpClient.setTimeout(2000);
 
-	if (energyAuthTokenExpired) {
-		energyFetchAuthToken(&httpClient);
-	}
+  if (energyAuthTokenExpired) {
+    energyFetchAuthToken(&httpClient);
+  }
 
-	energyFetchActualGen(&httpClient);
+  energyFetchActualGen(&httpClient);
 
-	return true;
+  return true;
 }
