af68178b335403e13675c04813fda711a47db0e6
maurinElectroTextile <maurin@datapaulette.org>
2018-11-09 15:25:14 +0100

    Updated but not tested

--- MultiClicks_ATTINY_MASTER/MultiClicks_ATTINY_MASTER.ino
+++ MultiClicks_ATTINY_MASTER/MultiClicks_ATTINY_MASTER.ino
@@ -2,23 +2,34 @@
 
 #include <Wire.h>
 
-#define SLAVE_ADDR 0x27
+#define SLAVE_ADDR  0x27
+#define ANSWER_PIN  7
+
+boolean setMode = false;
 
 void setup() {
   Serial.begin(115200);                    // petit debug via le port série
   Serial.println("I2C Test Application");  // hop on affiche le start (pratique sur les reset)
+  attachInterrupt(digitalPinToInterrupt(ANSWER_PIN), requestMode, RISING);
   Wire.begin();                            // I2C bus is on pin D1
-
 }
 
 void loop() {
-  Wire.beginTransmission(SLAVE_ADDR); // transmit to device 0x27
-  Wire.write('A');                    // Request 
-  Wire.endTransmission();             // stop transmitting
 
-  while (Wire.available()) {          // slave may send less than requested
-    char c = Wire.read();             // receive a byte as character
-    Serial.print(c);                  // print the character
+
+  if (setMode) {
+    Wire.beginTransmission(SLAVE_ADDR); // transmit to device 0x27
+    Wire.write('A');                    // Request
+    Wire.endTransmission();             // stop transmitting
+
+    while (Wire.available()) {          // slave may send less than requested
+      char c = Wire.read();             // receive a byte as character
+      Serial.print(c);                  // print the character
+    }
+    setMode = false;
   }
-  delayMicroseconds(1000);            // on glande une seconde
+}
+
+void requestMode() {
+  setMode = true;
 }
