8a47be17045f2f85a7747cc57adf0f0c4c10973c
Nicolas Thill <nicolas.thill@gmail.com>
2018-11-10 18:25:05 +0100

    add a ts (timestamp) field to serializers

--- server/src/data_api_v1/serializers.py
+++ server/src/data_api_v1/serializers.py
@@ -7,13 +7,23 @@ from .models import EnergyMeasure, WeatherMeasure
 
 
 class EnergyMeasureSerializer(serializers.ModelSerializer):
+    ts = serializers.SerializerMethodField()
+
+    def get_ts(self, obj):
+        return int(obj.dt.timestamp())
+
     class Meta:
         model = EnergyMeasure
-        fields = ('id', 'dt', 'fossil_ratio', 'nuclear_ratio', 'renewable_ratio')
+        fields = ('id', 'dt', 'ts', 'fossil_ratio', 'nuclear_ratio', 'renewable_ratio')
 
 
 class WeatherMeasureSerializer(serializers.ModelSerializer):
+    ts = serializers.SerializerMethodField()
+
+    def get_ts(self, obj):
+        return int(obj.dt.timestamp())
+
     class Meta:
         model = WeatherMeasure
-        fields = ('id', 'dt', 'temp', 'temp_min', 'temp_max', 'cond_id', 'cond_text', 'description')
+        fields = ('id', 'dt', 'ts', 'temp', 'temp_min', 'temp_max', 'cond_id', 'cond_text', 'description')
 
