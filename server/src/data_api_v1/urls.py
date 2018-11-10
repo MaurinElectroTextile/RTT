c327b6b74906e776cbfaa84981903a68af506bc5
Nicolas Thill <nicolas.thill@gmail.com>
2018-11-10 17:24:28 +0100

    server: add API root endpoint

--- server/src/data_api_v1/urls.py
+++ server/src/data_api_v1/urls.py
@@ -8,6 +8,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('energy/', views.EnergyMeasureList.as_view(), name = 'energy-list'),
     path('energy/<int:pk>/', views.EnergyMeasureDetail.as_view(), name = 'energy-detail'),
     path('weather/', views.WeatherMeasureList.as_view(), name = 'weather-list'),
--- server/src/data_api_v1/views.py
+++ server/src/data_api_v1/views.py
@@ -4,9 +4,20 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from .models import EnergyMeasure, WeatherMeasure
 from .serializers import EnergyMeasureSerializer, WeatherMeasureSerializer
+
+
+@api_view(['GET'])
+def index(request, format = None):
+    return Response({
+        'energy': reverse('energy-list', request = request, format = format),
+        'weather': reverse('weather-list', request = request, format = format)
+    })
 
 
 class EnergyMeasureList(generics.ListAPIView):
