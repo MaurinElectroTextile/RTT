dee102f8d2a5f3dd759f98ecc584ff466b3ad043
Nicolas Thill <nicolas.thill@gmail.com>
2018-11-11 23:31:09 +0100

    server: update models

--- server/src/data_api_v1/models.py
+++ server/src/data_api_v1/models.py
@@ -5,29 +5,29 @@ from django.db import models
 
 
 class EnergyMeasure(models.Model):
-    dt = models.DateTimeField('update date', unique = True)
+    d = models.DateField('update date')
+    dt = models.DateTimeField('update datetime')
     fossil_ratio = models.DecimalField('fossil ration', max_digits = 3, decimal_places = 2, default = 0.0)
     nuclear_ratio = models.DecimalField('nuclear ration', max_digits = 3, decimal_places = 2, default = 0.0)
     renewable_ratio = models.DecimalField('renewable ratio', max_digits = 3, decimal_places = 2, default = 0.0)
 
     class Meta:
         indexes = [
-            models.Index(fields = [ 'dt' ]),
+            models.Index(fields = [ 'd', 'dt' ]),
         ]
         ordering = [ '-dt' ]
 
 
 class WeatherMeasure(models.Model):
-    dt = models.DateTimeField('update date', unique = True)
+    d = models.DateField('update date')
+    dt = models.DateTimeField('update datetime')
     temp = models.DecimalField('temperature', max_digits = 5, decimal_places = 2, default = 0.0)
-    temp_min = models.DecimalField('minimum temperature', max_digits = 5, decimal_places = 2, default = 0.0)
-    temp_max = models.DecimalField('maximum temperature', max_digits = 5, decimal_places = 2, default = 0.0)
     cond_id = models.IntegerField('condition id', default = 0)
     cond_text = models.TextField('condition text', blank = True, default = '')
     description = models.TextField('description', blank = True, default = '')
 
     class Meta:
         indexes = [
-            models.Index(fields = [ 'dt' ]),
+            models.Index(fields = [ 'd', 'dt' ]),
         ]
         ordering = [ '-dt' ]
