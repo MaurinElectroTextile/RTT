3be2a528c2d1d50910cb98d6b02ab90588063f71
Nicolas Thill <nicolas.thill@gmail.com>
2018-11-12 02:29:47 +0100

    server: return weather data in combined API

--- server/src/data_api_v1/views.py
+++ server/src/data_api_v1/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from datetime import datetime, date, timedelta
 
+from django.db.models import Min, Max
 from django.shortcuts import render
+from django.utils import timezone
 
 from rest_framework import generics
 from rest_framework.decorators import api_view
@@ -51,11 +53,82 @@ class WeatherMeasureDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = WeatherMeasureSerializer
 
 
+def updateWeather(jo):
+    dt = datetime.fromtimestamp(jo['dt'], timezone.utc)
+    d = dt.date()
+    try:
+        wm = WeatherMeasure.objects.get(dt = dt)
+    except WeatherMeasure.DoesNotExist:
+        wm = WeatherMeasure(d = d, dt = dt)
+    wm.temp = jo['main']['temp']
+    wm.cond_id = jo['weather'][0]['id']
+    wm.cond_text = jo['weather'][0]['main']
+    wm.description = jo['weather'][0]['description']
+    wm.save()
+    return wm
+
+
+def getCurrentWeather():
+    jo = fetchWeatherForecast()
+    if 'list' in jo:
+        for je in jo['list']:
+            updateWeather(je)
+    jo = fetchWeatherCurrent()
+    dt = datetime.fromtimestamp(jo['dt'], timezone.utc)
+    d = dt.date()
+    wm = updateWeather(jo)
+    ws = WeatherMeasureSerializer(wm)
+    jd = ws.data
+    jd['temp_min'] = WeatherMeasure.objects.filter(d__exact = d).aggregate(Min('temp'))['temp__min']
+    jd['temp_max'] = WeatherMeasure.objects.filter(d__exact = d).aggregate(Max('temp'))['temp__max']
+    return jd
+
+
+def getTomorrowWeather():
+    dt = timezone.now() + timedelta(days = 1)
+    d = dt.date()
+    jo = fetchWeatherForecast()
+    if 'list' in jo:
+        for je in jo['list']:
+            updateWeather(je)
+    try:
+        wm = WeatherMeasure.objects.filter(d__exact = d, dt__lte = dt).order_by('-dt')[0]
+    except IndexError:
+        jd = {}
+        jd['error'] = {}
+        jd['error']['code'] = 1
+        jd['error']['message'] = 'no data'
+        return jd
+    ws = WeatherMeasureSerializer(wm)
+    jd = ws.data
+    jd['temp_min'] = WeatherMeasure.objects.filter(d__exact = d).aggregate(Min('temp'))['temp__min']
+    jd['temp_max'] = WeatherMeasure.objects.filter(d__exact = d).aggregate(Max('temp'))['temp__max']
+    return jd
+
+
+def getYesterdayWeather():
+    dt = timezone.now() - timedelta(days = 1)
+    d = dt.date()
+    try:
+        wm = WeatherMeasure.objects.filter(d__exact = d, dt__lte = dt).order_by('-dt')[0]
+    except IndexError:
+        jd = {}
+        jd['error'] = {}
+        jd['error']['code'] = 1
+        jd['error']['message'] = 'no data'
+        return jd
+    ws = WeatherMeasureSerializer(wm)
+    jd = ws.data
+    jd['temp_min'] = WeatherMeasure.objects.filter(d__exact = d).aggregate(Min('temp'))['temp__min']
+    jd['temp_max'] = WeatherMeasure.objects.filter(d__exact = d).aggregate(Max('temp'))['temp__max']
+    return jd
+
+
 @api_view(['GET'])
 def get_combined_today(request, format = None):
     jr = {}
     jr['data'] = {}
-    jr['data']['weather'] = {}
+    jr['data']['weather'] = getCurrentWeather()
     jr['data']['energy'] = {}
     return Response(jr)
 
@@ -64,7 +137,7 @@ def get_combined_today(request, format = None):
 def get_combined_tomorrow(request, format = None):
     jr = {}
     jr['data'] = {}
-    jr['data']['weather'] = {}
+    jr['data']['weather'] = getTomorrowWeather()
     jr['data']['energy'] = {}
     return Response(jr)
 
@@ -73,7 +146,7 @@ def get_combined_tomorrow(request, format = None):
 def get_combined_yesterday(request, format = None):
     jr = {}
     jr['data'] = {}
-    jr['data']['weather'] = {}
+    jr['data']['weather'] = getYesterdayWeather()
     jr['data']['energy'] = {}
     return Response(jr)
 
