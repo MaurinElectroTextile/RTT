f6d0a60995b9cb45478c6b6477edcbd846505bc5
Nicolas Thill <nicolas.thill@gmail.com>
2018-11-14 16:33:30 +0100

    server/settings: don't coerce decimals to strings in serializers

--- server/src/settings.py
+++ server/src/settings.py
@@ -112,6 +112,7 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
